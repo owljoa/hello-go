42f87ee3e2165dabef0b428256bc6dee5a081fe3
Dong In Kim <needshield@gmail.com>
2021-05-05 15:46:34 +0900

    add logic for checking input is empty string

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Returns a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// if a name was received, return a value that embeds the name
+	// in a greeting messagae
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
