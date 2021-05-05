3a315bd9bf12a48e0e7c8b3784b669417a4e066c
Dong In Kim <needshield@gmail.com>
2021-05-05 16:02:51 +0900

    add error handling logic to hello module

--- hello-module/hello.go
+++ hello-module/hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/owljoa/hello-go/greetings"
 )
 
 func main() {
-	// Get a greetings message and print it.
-	message := greetings.Hello("Owljoa")
+	// Set properties of the predefined Logger. including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("")
+	// if an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
